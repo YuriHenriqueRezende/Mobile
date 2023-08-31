0c2ac192ee9d6fe67d68d394114e2be17c8404ba
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-08-30 22:05:23 -0300

    Update aula.dart

--- dart/aula.dart
+++ dart/aula.dart
@@ -1,4 +1,5 @@
-/*void escreve_msg() {
+/*
+void escreve_msg() {
   print("Senai_ADS");
 }
 
@@ -6,3 +7,23 @@ void main() {
   escreve_msg();
 }
 */
+/*
+double calc_salario(double salario, int bonus)=> salario - (0.1*salario) +bonus;
+void main()
+{
+ double total_s;
+ total_s = calc_salario(5000,2000);
+ print('Salario total R $total_s');
+}
+*/
+void exibir_dados(String nome,{ int? idade, double? h}) // Cria função com 
+{
+ print("Nome $nome");
+ print("Idade: $idade");
+ print("Altura: $h");
+}
+void main()
+{
+ exibir_dados("Yuri", idade:19, h:1.80); // passagem de parâmetros opcionais 
+}
+
