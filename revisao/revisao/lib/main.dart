4ed3a58fec6148044aaa71d0c435dc0d291d4dfc
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-08 19:43:34 -0300

    commit

---
+++ revisao/revisao/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+    ),
+  )
+}
