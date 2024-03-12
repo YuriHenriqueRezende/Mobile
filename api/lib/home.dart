05848c5c8b0d68fdf95f11e684047d4e8f6def97
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-03-12 20:19:31 -0300

    commit

--- api/lib/home.dart
+++ api/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -8,6 +10,32 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  _consultar () async {
+    String url = "https://viacep.com.br/ws/01311923/json/";
+  http.Response response;
+  response = await http.get(Uri.parse(url));
+  print("codigo ${response.statusCode.toString()}");
+  print("API ${response.body}");
+  String ? logradouro;
+  String ? bairro;
+  String ? cidade;
+  String ? estado;
+  String ? ddd;
+
+  Map<String,dynamic> dados = json.decode(response.body);
+  logradouro = dados["logradouro"];
+  print("logradouro $logradouro");
+
+  bairro = dados["bairro"];
+  print("bairro $bairro");
+
+  cidade = dados["localidade"];
+  print("cidade $cidade");
+
+  ddd = dados["ddd"];
+  print("ddd $ddd");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +44,8 @@ class _homeState extends State<home> {
       ),
       body: Column(
         children: [
-          
+          ElevatedButton(onPressed: _consultar, child: Text("Clique"))
+
         ],
       ),
     );
